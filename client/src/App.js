0f292bb9dbd0d9bb925e6510e87159107735f6ad
auwilson92 <auwilson92@gmail.com>
2021-01-27 22:49:30 -0800

    edited home link

--- client/src/App.js
+++ client/src/App.js
@@ -17,7 +17,7 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            This is a test
+            Learn React
           </a>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
